ec4fe14920124a9dbc9e9223abb531dee2563abc
Matthew Francis <mattfrancis888@gmail.com>
2020-05-31 16:39:16 -0400

    Set up GraphQL/Apollo, and Express server for React. The server itneracts with JSON-server's db.json; added GraphQL queries to get list of users, a specific user and a specific company. Also created small daata in db,json.

---
+++ backend/schema.js
@@ -0,0 +1,73 @@
+const graphql = require("graphql");
+const axios = require("axios");
+
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema,
+    GraphQLList,
+    GraphQLNonNull,
+} = graphql;
+
+const CompanyType = new GraphQLObjectType({
+    name: "Company",
+    fields: () => ({
+        //need to be wrapped in ()=>(); becuase javascript would execute this block after everything in the file has loaded
+        //this is because of how closures work. Needed to be done becausse it's referencing
+        //UserType; Usertype hasnt been declared yet
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+    }),
+});
+
+const UserType = new GraphQLObjectType({
+    name: "User", //Just indicator that its an objector else code it wont recognize the object
+
+    //need to be wrapped in ()=>(); becuase javascript would execute this block after everything in the file has loaded
+    //this is because of how closures work.
+    fields: () => ({
+        id: { type: GraphQLString },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+    }),
+});
+
+// const users = [{ id: "23", firstName: "Bill", age: 20 }];
+//Able to get id for user or company
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        users: {
+            type: new GraphQLList(UserType),
+            resolve() {
+                return axios
+                    .get("http://localhost:3000/users")
+                    .then((response) => response.data);
+            },
+        },
+        user: {
+            type: UserType, //needed so you access user userType properties
+            args: { id: { type: GraphQLString } },
+
+            resolve(parentValue, args) {
+                return axios
+                    .get(`http://localhost:3000/users/${args.id}`)
+                    .then((response) => response.data);
+            },
+        },
+        company: {
+            type: CompanyType,
+            args: { id: { type: GraphQLString } },
+            resolve(parentValue, args) {
+                // return users.find((user) => user.id === args.id);
+                return axios
+                    .get(`http://localhost:3000/companies/${args.id}`)
+                    .then((response) => response.data);
+            },
+        },
+    },
+});
+
+module.exports = new GraphQLSchema({ query: RootQuery });
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,43 @@
-const jsonServer = require("json-server");
+// const jsonServer = require("json-server");
 
-var path = require("path");
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 5000;
+// var path = require("path");
+// const middlewares = jsonServer.defaults();
+// const port = process.env.PORT || 4000;
 
-const router = jsonServer.router(path.join(__dirname, "db.json"));
-const db = router.db.getState();
+// const router = jsonServer.router(path.join(__dirname, "db.json"));
+// const db = router.db.getState();
 
-const server = jsonServer.create();
-server.use(middlewares);
+// const expressGraphQL = require("express-graphql");
+// const schema = require("./schema");
 
-server.use(router);
-server.listen(port, () => {
-    console.log("JSON Server is running");
+// const server = jsonServer.create();
+// server.use(middlewares);
+
+// server.use(router);
+// server.use(
+//     "/graphql",
+//     expressGraphQL({
+//         schema,
+//         graphiql: true,
+//     })
+//     //graphiql is the development tool for grahql elements
+// );
+// server.listen(port, () => {
+//     console.log("JSON Server is running");
+// });
+
+const express = require("express");
+const expressGraphQL = require("express-graphql");
+const schema = require("./schema");
+const app = express();
+app.use(
+    "/graphql",
+    expressGraphQL({
+        schema,
+        graphiql: true,
+    })
+    //graphiql is the development tool for grahql elements
+);
+app.listen(4000, () => {
+    console.log("Listening");
 });
